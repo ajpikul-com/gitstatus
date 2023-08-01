8ab1a47a1e66ca712ee1e98325e8a8bcf5ba6019
Andrew Pikul <ajpikul@gmail.com>
2023-08-01 23:43:46 +0000

    Commiting just to save this work which is too complicated

--- status.go
+++ status.go
@@ -2,6 +2,8 @@ package gitstatus
 
 import (
 	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
+	"github.com/go-git/go-git/v5/plumbing/object"
 )
 
 type repoState struct {
@@ -30,7 +32,8 @@ func VerifyRepos() {
 				defaultLogger.Error("Error getting status")
 			}
 			if !status.IsClean() {
-				defaultLogger.Debug("There are changes here to be commited")
+				defaultLogger.Debug("Not Clean")
+				defaultLogger.Debug(status.String())
 			}
 			remotes, err := repo.Remotes()
 			if err != nil {
@@ -39,7 +42,41 @@ func VerifyRepos() {
 			}
 			if len(remotes) == 0 {
 				defaultLogger.Debug("This repo has no remote!")
-			} // else fetch
+			} else {
+				for _, remote := range remotes {
+					err = remote.Fetch(&git.FetchOptions{})
+					if err != nil && err == git.NoErrAlreadyUpToDate {
+						defaultLogger.Error(err.Error())
+						continue
+					}
+					if remote.Config().Name == "origin" {
+						upstreamRef, err := repo.Reference(plumbing.NewRemoteHEADReferenceName("origin"), true)
+						if err != nil {
+							panic(err.Error())
+						}
+						options := &git.LogOptions{
+							From:  upstreamRef.Hash(),
+							Order: git.LogOrderCommitterTime,
+						}
+						iter, err := repo.Log(options)
+						if err != nil {
+							panic(err.Error())
+						}
+						err = iter.ForEach(func(commit *object.Commit) error {
+							defaultLogger.Debug(commit.Hash.String()) // check to see if we are that house
+							defaultLogger.Debug(commit.Message)
+							return nil
+						})
+						if err != nil {
+							panic(err.Error())
+						}
+					}
+				}
+				// It has remotes and we've fetched them
+				// We now want to know if we're behind from upstream
+				// But go-git provides no easy way to do that
+				// We'd have to get a reference to the upstream head
+			}
 		}
 	}
 }
